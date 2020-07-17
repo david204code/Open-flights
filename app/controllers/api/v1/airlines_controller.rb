eabd52baf78472bdd90e33502e4627b5e7070dc0
David Wu <david204code@gmail.com>
2020-07-17 19:18:50 +0100

    Add the CRUD action for the AirlinesController

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,6 +1,64 @@
 module Api
   module V1
     class AirlinesController < ApplicationController
+      
+      # get all the airlines from the database
+      def index
+        airlines = Airline.all 
+
+        render json: AirlinesSerializer.new(airlines, options).serialized_json
+      end
+
+      # get individual airline from the database
+
+      def show
+        airline = Airline.find_by(slug: params[:slug])
+
+        render json: AirlinesSerializer.new(airline, options).serialized_json
+      end
+
+      def create
+        airline = Airline.new(airline_params)
+
+        if airline.save
+          render json: AirlinesSerializer.new(airline).serialized_json
+        else
+          render json: { error: airline.errors.messages }, status: 422
+        end
+      end
+
+      def update
+        airline = Airline.find_by(slug: params[:slug])
+
+        if airline.update(airline_params)
+          render json: AirlinesSerializer.new(airline, options).serialized_json
+        else
+          render json: { error: airline.errors.messages }, status: 422
+        end
+      end
+
+      def destroy
+        airline = Airline.find_by(slug: params[:slug])
+
+        if airline.destroy
+          head :no_content
+        else
+          render json: { error: airline.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      # whitelist of the airline parameters
+      def airline_params
+        params.require(:airline).permit(:name, :image_url)
+      end
+
+      # specify the resources that want to be included
+      def option
+        @options ||= { include: %i[reviews]}
+      end
+
     end
   end
 end
