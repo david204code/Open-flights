51b4644b02122e9f426f2df3e7129d6cabc82ace
David Wu <david204code@gmail.com>
2020-07-20 23:09:44 +0100

    Created a new Header component for the Airline component

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './Header';
 
 const Airline = (props) => {
   // Object, set initial set to empty object
@@ -12,10 +14,25 @@ const Airline = (props) => {
     const slug = props.match.params.slug
     const url = `/api/v1/airlines/${slug}`
     console.log(props);
+
+    axios.get(url)
+    .then( resp => setAirline(resp.data) )
+    .catch( resp => console.log(resp) )
+
     // pass in an empty argument/array so it only runs once
   }, [])
 
-  return <div>This is the Airlines#show view for our app</div>
+  return (
+    <div className ="wrapper">
+      <div className ="column">
+        <div className ="header"></div>
+        <div className ="reviews"></div>
+      </div>
+      <div className ="column">
+        <div className ="review-form">[Review form goes here.]</div>
+      </div>
+    </div>
+  )
 }
 
 export default Airline;
