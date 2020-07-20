b3059ca0a8c4eca831e9a9c33610bc6e824d8f74
David Wu <david204code@gmail.com>
2020-07-20 18:08:06 +0100

    yarn add axios AND added React Hooks

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Airlines = () => {
-  return <div>This is the Airlines#index view for our app.</div>
+  const [airlines, setAirlines] = useState([])
+
+  useEffect(() => {
+    //Get all airlines from api
+    //update airlines in our state
+  })
+
+
+  return (
+    <div>This is the Airlines#index view for our app.</div>
+  )
 }
 
 export default Airlines;
