0b8f5f902dc233df24f67001d58730fb069e7e83
David Wu <david204code@gmail.com>
2020-07-20 22:58:31 +0100

    Layed the foundation to get idividual airline data on React

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const Airline = () => {
+const Airline = (props) => {
+  // Object, set initial set to empty object
+  const [airline, setAirline] = useState({})
+  const [review, setReview] = useState({})
+
+  useEffect(() => {
+    // api/v1/airlines/:slug
+    // need to get the :slug value for React, 
+    // get that value from URL
+    const slug = props.match.params.slug
+    const url = `/api/v1/airlines/${slug}`
+    console.log(props);
+    // pass in an empty argument/array so it only runs once
+  }, [])
+
   return <div>This is the Airlines#show view for our app</div>
 }
 
