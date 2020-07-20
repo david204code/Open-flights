76298e152ebc8096336db4c402885a8360fc700d
David Wu <david204code@gmail.com>
2020-07-20 19:43:29 +0100

    Format the app display

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -3,6 +3,34 @@ import axios from 'axios';
 import Airline from './Airline';
 import styled from 'styled-components';
 
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+
+  h1 {
+    font-size: 42px;
+  }
+`
+
+const Subheader = styled.div`
+  font-weight: 300;
+  font-size: 20px;
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding: 20px;
+`
+
 const Airlines = () => {
   const [airlines, setAirlines] = useState([])
 
@@ -29,15 +57,15 @@ const Airlines = () => {
   })
 
   return (
-    <div className ="home">
-      <div className ="header">
+    <Home>
+      <Header>
         <h1>OpenFlights</h1>
-        <div className ="subheader">Honest, unbiased airlines reviews.</div>
-      </div>
-      <div className ="grid">
+        <Subheader>Honest, unbiased airlines reviews.</Subheader>
+      </Header>
+      <Grid>
         {grid}
-      </div>
-    </div>
+      </Grid>
+    </Home>
 
   )
 }
