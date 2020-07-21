f0aaa15fe616bdb0b3212d104ce0e7f92d290f76
David Wu <david204code@gmail.com>
2020-07-21 17:10:53 +0100

    Creat new ReviewForm component and imported in the Airpline component

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
 import Header from './Header';
 import styled from 'styled-components';
+import ReviewForm from './ReviewForm';
 
 const Wrapper = styled.div`
   margin-left: auto;
@@ -23,8 +24,6 @@ const Coloumn = styled.div`
 const Main = styled.div`
   padding-left: 50px;
 `
-
-
 
 const Airline = (props) => {
   // Object, set initial set to empty object
@@ -53,23 +52,25 @@ const Airline = (props) => {
 
   return (
     <Wrapper>
-      <Coloumn>
-        <Main>
-          { 
-          // check that the data is loaded
-            loaded && 
-            <Header
-            // passing data to the Header components
+      { 
+      // check that the data is loaded
+        loaded && 
+        <Fragment>
+          <Coloumn>
+            <Main>
+              <Header
+              // passing data to the Header components
               attributes ={airline.data.attributes}
               reviews ={airline.included}
-            />
-          } 
-          <div className ="reviews"></div>
-        </Main>
-      </Coloumn>
-      <Coloumn>
-        <div className ="review-form">[Review form goes here.]</div>
-      </Coloumn>
+              />
+              <div className ="reviews"></div>
+            </Main>
+          </Coloumn>
+          <Coloumn>
+            <ReviewForm/>
+          </Coloumn>          
+        </Fragment>
+      } 
     </Wrapper>
   )
 }
